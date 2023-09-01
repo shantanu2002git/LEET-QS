0a878f0bbb3d324248915462a148f409f28c6ebf
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2023-09-01 22:19:46 +0530

    Time: 9 ms (44.33%) | Memory: 5.8 MB (95.40%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        int val[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        string symbol[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};;
+        string si;
+        for(int i=0; i<13; i++){
+            while(num>=val[i]){
+                si.append(symbol[i]);
+                num=num-val[i];
+            }
+        }
+        return si;
+    }
+};

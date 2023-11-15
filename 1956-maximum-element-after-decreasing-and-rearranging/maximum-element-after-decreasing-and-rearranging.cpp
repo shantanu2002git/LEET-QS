c852a95160a4c4e3668176ef560251a5cac5008e
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2023-11-15 10:43:16 +0530

    Time: 89 ms (12.69%) | Memory: 51.5 MB (56.72%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -4,22 +4,22 @@ public:
     int maximumElementAfterDecrementingAndRearranging(vector<int> &arr)
     {
         sort(arr.begin(), arr.end());
-        int n = arr.size(), mx = -1;
+        int n = arr.size(), mx = 1;
         arr[0]=1;
         for (int i = 1; i < n; i++)
         {
             if (abs(arr[i] - arr[i - 1]) <= 1)
             {
-                //mx = max(mx, max(arr[i], arr[i - 1]));
+                mx = max(mx, max(arr[i], arr[i - 1]));
                 continue;
             }
             arr[i] = arr[i - 1] + 1;
-           // mx = max(mx, max(arr[i], arr[i - 1]));
+           mx = max(mx, max(arr[i], arr[i - 1]));
         }
-        for(auto it : arr){
-            cout<<it<<" ";
-        }
-        mx=*max_element(arr.begin(),arr.end());
+        // for(auto it : arr){
+        //     cout<<it<<" ";
+        // }
+        // mx=*max_element(arr.begin(),arr.end());
         return mx;
     }
     };

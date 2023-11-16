dde651ca33a1b35dbea6e931b5cdab14ca6de06b
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2023-11-16 20:30:39 +0530

    Time: 97 ms (29.94%) | Memory: 50.3 MB (9.24%) - LeetSync

--- 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.cpp
+++ 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.cpp
@@ -1,33 +1,47 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& qre) {
+    vector<int> sumEvenAfterQueries(vector<int> &nums, vector<vector<int>> &qre)
+    {
         int n = nums.size();
         int evs = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] % 2 == 0) {
-                evs += nums[i];
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] % 2 == 0)
+            {
+                evs = evs + nums[i];
             }
         }
         vector<int> res;
-        for (auto it : qre) {
+        for (auto it : qre)
+        {
             int vl = it[0], idx = it[1];
-            if (nums[idx] % 2 == 0 && vl % 2 == 0) { // ev == ev
-                evs += vl;
-            } else if (nums[idx] % 2 != 0 && vl % 2 != 0) { // od == od
+            if (nums[idx] % 2 == 0 && vl % 2 == 0) //ev==ev
+            {
+                evs = evs + vl;
+            }
+           else if (nums[idx] % 2 != 0 && vl % 2 != 0)//od==od
+            {
                 evs += (nums[idx] + vl);
-            } else if (nums[idx] % 2 == 0 && vl % 2 != 0) { // ev == od
-                evs -= nums[idx];
-            } else if (nums[idx] % 2 != 0 && vl % 2 == 0) { // od == ev
-                if ((nums[idx] + vl) % 2 == 0) {
-                    if (vl < 0) {
-                        evs += vl;
-                    } else {
-                        evs -= vl;
-                    }
-                }
+            }
+            else if(nums[idx]%2==0 && vl%2!=0){ // ev==od
+                evs=evs-nums[idx];
+                
+            }
+            else if(nums[idx]%2!=0 && vl%2==0){ //od==ev
+
+             if ((nums[idx] + vl) % 2 == 0)
+        {
+            if(vl<0){
+ evs=evs+(vl);
+            }else{
+                evs=evs-vl;
+            }
+               
+            }
             }
             res.push_back(evs);
-            nums[idx] += vl;
+             nums[idx] += vl;
         }
         return res;
     }

7a5b2c71736e6a93734f7d4cd585ba9e18e25757
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2023-11-16 10:40:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (98.79%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,43 +1,21 @@
-class Solution
-{
+class Solution {
 public:
-    set<string> st;
+    string findDifferentBinaryString(vector<string>& nums) {
+        string ans = "";
 
-    string cal(int idx, char bin[2], int n, string s)
-    {
-        if (idx >= n)
-        {
-            if (!st.count(s))
-            {
-                return s;
-            }
-            return "";
+        for (int i = 0; i < nums.size(); ++i) {
+            char currentChar = nums[i][i];
+            char oppositeChar = (currentChar == '0') ? '1' : '0';
+
+            // add the opposite character to the result string
+            ans += oppositeChar;
         }
 
-        string s1 = s + bin[0];
-        string s2 = s + bin[1];
-
-        string res = cal(idx + 1, bin, n, s1);
-        if (!res.empty())
-        {
-            return res;
-        }
-        return cal(idx + 1, bin, n, s2);
-    }
-
-
-
-
-
-    string findDifferentBinaryString(vector<string> &nums)
-    {
-        for (auto it : nums)
-        {
-            st.insert(it);
-        }
-        int n = nums.size();
-        char bin[2] = {'0', '1'};
-        string s = "";
-        return cal(0, bin, n, s);
+        return ans;
     }
 };
+
+/*
+it will help help when this condition will follow  : 
+strings nums containing n unique binary strings each of length n
+*/

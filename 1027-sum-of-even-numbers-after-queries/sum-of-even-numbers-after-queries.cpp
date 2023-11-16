e358202bce72263c646ecda41c4dab54bd169de9
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2023-11-16 20:25:17 +0530

    Time: 104 ms (19.43%) | Memory: 50 MB (21.34%) - LeetSync

---
+++ 1027-sum-of-even-numbers-after-queries/sum-of-even-numbers-after-queries.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& qre) {
+        int n = nums.size();
+        int evs = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % 2 == 0) {
+                evs += nums[i];
+            }
+        }
+        vector<int> res;
+        for (auto it : qre) {
+            int vl = it[0], idx = it[1];
+            if (nums[idx] % 2 == 0 && vl % 2 == 0) { // ev == ev
+                evs += vl;
+            } else if (nums[idx] % 2 != 0 && vl % 2 != 0) { // od == od
+                evs += (nums[idx] + vl);
+            } else if (nums[idx] % 2 == 0 && vl % 2 != 0) { // ev == od
+                evs -= nums[idx];
+            } else if (nums[idx] % 2 != 0 && vl % 2 == 0) { // od == ev
+                if ((nums[idx] + vl) % 2 == 0) {
+                    if (vl < 0) {
+                        evs += vl;
+                    } else {
+                        evs -= vl;
+                    }
+                }
+            }
+            res.push_back(evs);
+            nums[idx] += vl;
+        }
+        return res;
+    }
+};

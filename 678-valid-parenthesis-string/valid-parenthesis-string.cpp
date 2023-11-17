c563ceb1785acf0655dee1addd2b08d570be8ada
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2023-11-17 10:49:54 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (76.43%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,23 +1,14 @@
 class Solution {
-public://NOT ME_____________________
+public:
     bool checkValidString(string s) {
-        int n = s.size();
-        int minOpen = 0, maxOpen = 0;
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '(') {
-                minOpen++;
-                maxOpen++;
-            } else if (s[i] == ')') {
-                minOpen = max(0, minOpen - 1);
-                maxOpen--;
-                if (maxOpen < 0) {
-                    return false;
-                }
-            } else {
-                minOpen = max(0, minOpen - 1);
-                maxOpen++;
-            }
-        }
-        return minOpen == 0;
+        int opn=0,cls=0;
+for(auto it : s){
+    opn+= (it=='(') ? 1 : -1;
+    cls+= (it!=')') ? 1 : -1;
+
+    if(cls<0) break;
+    opn=max(opn,0);
+}
+return opn==0;
     }
 };

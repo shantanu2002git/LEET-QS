71fd591b96186a144b4fe35a3250cf1d3708eb7c
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2023-11-23 18:57:27 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (30.02%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,6 +1,7 @@
 class Solution
 {
 public:
+/*
     // 226
     int calldp(string s, int idx, int n, vector<int> &dp)
     {
@@ -23,10 +24,36 @@ return dp[idx];
         }
         return dp[idx]=step;
     }
+    */
+
+
     int numDecodings(string s)
     {
+        /*
         int n = s.size();
         vector<int> dp(n + 1, -1);
         return calldp(s, 0, n, dp);
+        */
+        int n=s.size();
+        vector<int>dp(n+1,0);
+        dp[0]=1;
+        dp[1]=s[0]!='0' ? 1:0;
+        for(int idx=2; idx<=n; idx++){
+            if(s[idx-1]!='0')
+            {
+                dp[idx]=dp[idx]+dp[idx-1];
+            }
+            int tw=stoi(s.substr(idx-2,2));
+            if(tw>=10 && tw<=26){
+                dp[idx]+=dp[idx-2];
+            }
+        }
+        return dp[n];
     }
+
+
+
+
+
+
 };

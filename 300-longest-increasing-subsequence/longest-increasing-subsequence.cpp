13419d896603b82d82c166a698159af9203653f7
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-01-05 20:41:18 +0530

    Time: 971 ms (7.42%) | Memory: 292.7 MB (11.08%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,20 +1,3 @@
-/*class Solution {
-public:
-    
-    int lengthOfLIS(vector<int>& a) {
-         vector<int> piles; int n=a.size();
-        for (int i = 0; i < n; ++i) {
-            auto it = lower_bound(piles.begin(), piles.end(), a[i]);
-            if (it == piles.end())
-                piles.push_back(a[i]);
-            else
-                *it = a[i];
-        }
-        return piles.size();
-    }
-		
-};
-*/
 
 class Solution {
 public:
@@ -22,7 +5,8 @@ public:
         if( ind == nums.size()) return 0;
         
         if( dp[ind][prev+1] != -1) return dp[ind][prev+1]; 
-        if( prev ==-1 || nums[ind]> nums[prev]){
+        
+        if( prev ==-1 ||  nums[prev] < nums[ind]){
     return dp[ind][prev+1]= max( 1+ solve(nums,ind+1, ind, dp), solve( nums, ind+1, prev, dp));
         }
         

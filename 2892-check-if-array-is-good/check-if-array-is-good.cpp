82b8b28084a60ceb22a474d49a3a7e9fa9dcf770
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-01-16 14:54:20 +0530

    Time: 9 ms (33.65%) | Memory: 23.1 MB (70.52%) - LeetSync

---
+++ 2892-check-if-array-is-good/check-if-array-is-good.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+       int n=nums.size();
+        if(n==0 || n==1){
+            return 0;
+        }
+        sort(nums.begin(),nums.end());
+        if(nums[n-2]!=nums[n-1])
+        {
+            return 0;
+        }
+        for(int i=0; i<n-1; i++){
+            if(nums[i]!=i+1){
+                return 0;
+            }
+        }
+        return 1;
+    }
+};

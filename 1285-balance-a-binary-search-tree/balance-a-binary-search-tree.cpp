8f8d7231f1af51e24a864547673cc140e83a3143
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-01-17 15:16:20 +0530

    Time: 111 ms (47.32%) | Memory: 63.6 MB (58.38%) - LeetSync

---
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void call_val(TreeNode* root, vector<int>& val) {
+        if (root == nullptr) {
+            return;
+        }
+        call_val(root->left, val);
+        val.push_back(root->val);
+        call_val(root->right, val);
+    }
+
+    TreeNode* Tree(vector<int>& arr, int st, int en) {
+        if (st > en) {
+            return NULL;
+        }
+        int md = (st + en) / 2;
+        TreeNode* nod = new TreeNode(arr[md]);
+        nod->left = Tree(arr, st, md - 1);
+        nod->right = Tree(arr, md + 1, en);
+        return nod;
+    }
+
+    TreeNode* balanceBST(TreeNode* root) {
+        vector<int> val;
+        call_val(root, val);
+        int n = val.size();
+        return Tree(val, 0, n - 1);
+    }
+};

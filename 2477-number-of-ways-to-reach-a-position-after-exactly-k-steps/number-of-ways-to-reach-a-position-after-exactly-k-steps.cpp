6c45b8aea90eaaa8dd8e7ee1cdb5b62451af2563
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-01-18 10:10:33 +0530

    Time: 93 ms (70.52%) | Memory: 28.3 MB (69.90%) - LeetSync

---
+++ 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -0,0 +1,31 @@
+#include <cstring>
+
+class Solution {
+public:
+    int dp[5001][1001];
+    long long mod = 1e9 + 7;
+
+    int call_dp(int start, int end, int k) {
+        if (k == 0) {
+            if (start == end) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        if (dp[start + 1000][k] != -1) {
+            return dp[start + 1000][k];
+        }
+
+        long long lef = (call_dp(start - 1, end, k - 1) % mod) % mod; // left
+        long long rig = (call_dp(start + 1, end, k - 1) % mod) % mod; // right
+
+        return dp[start + 1000][k] = (lef + rig) % mod;
+    }
+
+    int numberOfWays(int start, int end, int k) {
+        memset(dp, -1, sizeof(dp)); 
+        return call_dp(start, end, k);
+    }
+};

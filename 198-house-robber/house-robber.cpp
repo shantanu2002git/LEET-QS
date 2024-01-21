f3211de45959e37e4a686543b33cd938e0cc36aa
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-01-21 14:58:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (9.67%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,3 +1,24 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size(),ttk,ntt;
+        vector<int>dp(n,0);
+        dp[0]=nums[0];
+
+        for(int i=1; i<n; i++){
+             ttk=nums[i];
+            if(i>1){
+                     ttk=ttk+dp[i-2];
+            }
+            ntt=0+dp[i-1];
+
+            dp[i]=max(ttk,ntt);
+        }
+        return dp[n-1];
+    }
+};
+
+/*
 
 class Solution {
 public:
@@ -9,6 +30,7 @@ public:
         } else if (idx < 0) {
             return 0;
         }
+
 if(dp[idx]!=-1){
     return dp[idx];
 }
@@ -16,8 +38,6 @@ if(dp[idx]!=-1){
             ttk =nums[idx]+call(nums, idx - 2,dp);
         ntt = 0+call(nums, idx-1,dp);
      
-        
-
         return dp[idx]=max(ntt, ttk);
     }
 
@@ -31,3 +51,4 @@ if(dp[idx]!=-1){
         return call(nums, n - 1, dp);
     }
 };
+*/

78056fd989e4576a71c7bb5312abbc8c1375dc7e
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-01-25 22:49:52 +0530

    Time: 11 ms (67.45%) | Memory: 13.2 MB (18.88%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& mat) {
-      int n=mat.size();
-      int ans=INT_MAX;
-      for(int i=1; i<n; i++ ){
-          for(int j=0; j<n; j++){
-              int lef,rig,mid;
-              if(j==0){
-                  rig=mat[i-1][j+1];
-                  mid=mat[i-1][j];
-                  mat[i][j]+=min(rig,mid);
-              }
-              else if(j==n-1){
-                  mid=mat[i-1][j];
-                  lef=mat[i-1][j-1];
-                  mat[i][j]+=min(mid,lef);
-              }
-              else {
-                  lef=mat[i-1][j-1];
-                  mid=mat[i-1][j];
-                  rig=mat[i-1][j+1];
-                  mat[i][j]+=min(lef,min(mid,rig));
-              }
-          }
-      } 
-      for(int i=0; i<n; i++){
-          ans=min(ans,mat[n-1][i]);
-      } 
-      return ans;
+        int n=mat.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        for(int i=0; i<n; i++){
+            dp[0][i]=mat[0][i];
+        }
+
+        for(int i=1; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(j==0){
+                    dp[i][j]=mat[i][j]+min(dp[i-1][j],dp[i-1][j+1]);
+                }else if(j==n-1){
+                    dp[i][j]=mat[i][j]+min(dp[i-1][j],dp[i-1][j-1]);
+                }else{
+                    dp[i][j]=mat[i][j]+min(dp[i-1][j],min(dp[i-1][j+1],dp[i-1][j-1]));
+                }
+            }
+        }
+int res=INT_MAX;
+        for(int c=0; c<n; c++ ){
+            res=min(res,dp[n-1][c]);
+        }
+        return res;
     }
 };

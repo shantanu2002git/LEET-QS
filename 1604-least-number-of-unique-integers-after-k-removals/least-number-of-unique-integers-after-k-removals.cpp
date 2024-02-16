8e6ec08b22287d22859a107191362b1346116973
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-02-16 21:06:45 +0530

    Time: 204 ms (34.18%) | Memory: 67.5 MB (41.25%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
-        map<int,int> mp;
-        for(auto it : arr){
+        map<int, int> mp;
+        for (auto it : arr) {
             mp[it]++;
         }
 
-        vector<pair<int,int>> vp(mp.begin(), mp.end());
+        vector<pair<int, int>> vp(mp.begin(), mp.end());
 
-        sort(vp.begin(),vp.end(),[](pair<int,int>& p1, pair<int,int>& p2){
+        sort(vp.begin(), vp.end(), [](pair<int, int>& p1, pair<int, int>& p2) {
             return p1.second < p2.second;
         });
 
-        for(auto it : vp){
-            int vl=it.first, f=it.second;
-            while(k > 0 && f > 0){
+        for (auto it : vp) {
+            int vl = it.first, f = it.second;
+            while (k > 0 && f > 0) {
                 k--;
                 f--;
-            }
-            if (f == 0) {
-                mp.erase(it.first);
+                if (f == 0) {
+                     mp.erase(it.first);
+                }
             }
         }
-        
-        return mp.size();
+
+            return mp.size();
     }
 };

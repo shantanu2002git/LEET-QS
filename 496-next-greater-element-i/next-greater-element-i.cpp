cdfd2a8cd1a71ed374765d5a3d33d1561b737892
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-02-22 14:55:44 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (37.15%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,27 +1,43 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums2.size(),mx=-1;
-        map<int,int>mp;
-        stack<int>st;
-        for(int i=n-1; i>=0; i--){
-          while(!st.empty() && st.top()<nums2[i]){
-              st.pop();
-          }
-
-          if(st.empty()){
-              mp[nums2[i]]=-1;
-          }
-          else{
-              mp[nums2[i]]=st.top();
-          }
-          st.push(nums2[i]);
-        }
-vector<int>res;
-        for(auto it : nums1){
-res.push_back(mp[it]);
-        }
-        return res;
-
+  vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
+  {
+    stack<int> st;
+    int n = nums2.size();
+    vector<int> res;
+    for (int i = n - 1; i >= 0; i--)
+    {
+      while (!st.empty() && st.top() <= nums2[i])
+      {
+        st.pop();
+      }
+      if (st.empty())
+      {
+        res.push_back(-1);
+      }
+      else
+      {
+        res.push_back(st.top());
+      }
+      st.push(nums2[i]);
     }
+    reverse(res.begin(), res.end());
+    for (auto it : res)
+    {
+      // cout << it << " ";
+    }
+    map<int, int> mp;
+    int k = 0;
+    for (auto it : nums2)
+    {
+      mp[it] = res[k++];
+    }
+    vector<int> returnVec;
+    for (auto it : nums1)
+    {
+      returnVec.push_back(mp[it]);
+    }
+    return returnVec;
+  }
 };

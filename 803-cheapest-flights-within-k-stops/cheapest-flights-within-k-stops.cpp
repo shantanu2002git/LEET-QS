470905ec0efd0993128e7550aaccfec050a209bc
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-02-23 08:47:07 +0530

    Time: 15 ms (78.73%) | Memory: 17.2 MB (23.47%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int>cost(n,INT_MAX);
-        vector<vector<pair<int,int>>>adj(n);
-        for(auto it : flights){
-            adj[it[0]].push_back({it[1],it[2]});
+    int findCheapestPrice(int n, vector<vector<int>>& fli, int src, int dst,
+                          int k) {
+        vector<vector<pair<int, int>>> adj(n );
+        for (auto it : fli) {
+            adj[it[0]].push_back({it[1], it[2]});
         }
-
-        queue<pair<int,pair<int,int>>>q;
-        cost[src]=0;
-        q.push({0,{src,0}});
-       
-       while(!q.empty()){
-           auto it =q.front();
-           q.pop();
-           int stp=it.first, nod=it.second.first, pric=it.second.second;
-           if(stp>k){
-               continue;
-           }
-           for(auto it : adj[nod]){
-               int to=it.first, prc=it.second;
-               if(pric+prc<cost[to]){
-                  cost[to]=pric+prc;
-                  q.push({stp+1,{to,cost[to]}});
-               }
-           }
-       }
-
-       if(cost[dst]==INT_MAX){
-           return -1;
-       }
-       return cost[dst];
+        queue<pair<int, pair<int, int>>> q;
+        vector<int> cost(n + 1, 1e9);
+        q.push({0, {src, 0}}); // cntp, src, cost;
+            cost[src] = 0;
+        while (!q.empty()) {
+            auto it = q.front();
+            q.pop();
+            int cp = it.first, from = it.second.first, cos = it.second.second;
+            if(cp>k){
+                continue;
+            }
+            for (auto it : adj[from]) {
+                int to = it.first, prc = it.second;
+                if (cos + prc < cost[to]) {
+                    cost[to] = cos + prc;
+                    q.push({cp + 1, {to, (cos + prc)}});
+                }
+            }
+        }
+        if (cost[dst] == 1e9) {
+            return -1;
+        }
+        return cost[dst];
     }
 };

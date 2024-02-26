ca40f6b418fde9d33843caeecb68e9d3419b00d5
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-02-26 11:57:18 +0530

    Time: 39 ms (73.99%) | Memory: 36.2 MB (49.45%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -1,18 +1,27 @@
+#include <vector>
+#include <algorithm> // for sort
+using namespace std;
 
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>arr(n+1,0);
         vector<int> res;
-        for (int i = 0; i < n; i++) {
-            arr[nums[i]] = 1;
+        
+        // Iterate over each number in the array
+        for(int i = 0; i < nums.size(); i++) {
+            int idx = abs(nums[i]) - 1; // Get the index corresponding to the number
+            
+            // If the number is positive, make the number at its index negative
+            if(nums[idx] > 0)
+                nums[idx] = -nums[idx];
         }
-        for (int i = 1; i <= n; i++) {
-            if (arr[i] == 0) {
-                res.push_back(i);
-            }
+        
+        // Iterate over the modified array to find indices with positive numbers
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] > 0) // If the number at index i is positive
+                res.push_back(i + 1); // Add i+1 to the result (1-indexed)
         }
+        
         return res;
     }
 };

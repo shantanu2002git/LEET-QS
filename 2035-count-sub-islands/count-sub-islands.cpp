fc387e9be5f7a16f25d30fbad734761d68526218
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-02-26 17:57:12 +0530

    Time: 213 ms (76.27%) | Memory: 95.4 MB (48.42%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.cpp
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -1,53 +1,37 @@
-/*
-
-    Time Complexity : O(N*M), where N is the number of rows and M is the number of columns of matrix(grid).
-    Space Complexity : O(N*M), Visited matrix space.
-
-    Solved Using Array + Matrix + DFS.
-
-*/
+#include <vector>
+using namespace std;
 
 class Solution {
-private: 
-    bool dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int row, int col, 
-    vector<vector<int>>& visited, int dxdy[][4], bool& flag){
-        int n = grid1.size();
-        int m = grid1[0].size();
-
-        if(grid1[row][col] == 0){
-            flag = false;
-        }
-        
-        visited[row][col] = 1;
-
-        for(int i=0; i<4; i++){
-            int newrow = row + dxdy[i][0];
-            int newcol = col + dxdy[i][1];
-            if(newrow>=0 && newcol>=0 && newrow<n && newcol<m && !visited[newrow][newcol] && 
-            grid2[newrow][newcol]){
-                dfs(grid1, grid2, newrow, newcol, visited, dxdy, flag);
-            }
-        } 
-        return flag;  
-    }
 public:
-    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        int n = grid1.size();
-        int m = grid1[0].size();
-        int dxdy[][4] = {{-1,0}, {0,1}, {1,0}, {0,-1}};
-        vector<vector<int>> visited(n, vector<int>(m,0));
+   void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int x, int y, bool &res) {
+    int m = grid1.size(), n = grid1[0].size();
+    if (x < 0 || y < 0 || x >= m || y >= n || grid2[x][y] == 0  ) return;
+    if ((grid1[x][y] == 0 && grid2[x][y] == 1)||(grid1[x][y] == 1 && grid2[x][y] == 0)) {
+        res = false;
+        return;
+    }
 
-        int count = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid2[i][j] == 1 && !visited[i][j]){
-                    bool flag = true;
-                    if(dfs(grid1, grid2, i, j, visited, dxdy, flag)){
-                        count++;
-                    }
+    grid2[x][y] = 0; grid1[x][y]=0;
+    
+    dfs(grid1, grid2, x + 1, y, res);
+    dfs(grid1, grid2, x, y + 1, res);
+    dfs(grid1, grid2, x - 1, y, res);
+    dfs(grid1, grid2, x, y - 1, res);
+}
+
+    
+    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        int m = grid1.size(), n = grid1[0].size();
+        int ans = 0;
+        for (int i = 0; i < m ; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid2[i][j] == 1) {
+                    bool res = true; 
+                    dfs(grid1, grid2, i, j, res);
+                    ans += res;
                 }
             }
         }
-        return count;
+        return ans;
     }
 };

364f6559faf29c1dc290ce99c851828a0a0ff9fa
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-03-04 10:57:48 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (55.08%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.cpp
+++ 985-bag-of-tokens/bag-of-tokens.cpp
@@ -13,6 +13,14 @@ public:
                 pow = pow + tok[j--];
                 sc--;
             } else {
+                /*when ele is work : 
+               1. when pow is very less than tokens[0] then we can not do anything also sc=0 so we can not enter the ele if() condition
+                so you can break there;
+
+                2. now think sc>=1 but pow 5 and remaining tok[0]=4..tok1[3,2,1] on this time also we can not do any hting pow> tok{}
+
+                3. 
+                */
                 break;
             }
         }

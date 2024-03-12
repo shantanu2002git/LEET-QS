0e9b346fed23141ca7f5ce7fe05e1b4979cf58a5
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-03-12 19:05:31 +0530

    Time: 226 ms (82.46%) | Memory: 143.9 MB (77.19%) - LeetSync

--- 2289-minimum-operations-to-make-the-array-alternating/minimum-operations-to-make-the-array-alternating.cpp
+++ 2289-minimum-operations-to-make-the-array-alternating/minimum-operations-to-make-the-array-alternating.cpp
@@ -1,46 +1,22 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
+        unordered_map<int,int>odd,even;
+        int omax=0,emax=0,somax=0,semax=0,oval,eval;
+        for(int i=0;i<nums.size();i++){
+            if(i%2==0)
+            even[nums[i]]+=1;
+            else odd[nums[i]]+=1;
+        }
+        for(auto i:odd) if(i.second>omax){ omax=i.second; oval=i.first;}           
+        for(auto i:even)if(i.second>emax){ emax=i.second; eval=i.first; }
 
-        int n = nums.size();
-        unordered_map<int, int> even;
-        int even_max_freq = 0, even_max_element = 0, even_sec_max_freq = 0,
-            even_sec_max_element = 0;
-        // Largest and SecondLargest Frequency element at even index
-        for (int i = 0; i < n; i = i + 2) {
-            even[nums[i]]++;
-            if (even[nums[i]] > even_max_freq) {
-                even_max_element = nums[i];
-                even_max_freq = even[nums[i]];
-            } else if (even[nums[i]] <= even_max_freq &&
-                       nums[i] != even_max_element &&
-                       even_sec_max_freq < even[nums[i]]) {
-                even_sec_max_element = nums[i];
-                even_sec_max_freq = even[nums[i]];
-            }
-        }
-        unordered_map<int, int> odd;
-        int odd_max_freq = 0, odd_max_element = 0, odd_sec_max_freq = 0,
-            odd_sec_max_element = 0;
-        // Largest and SecondLargest Frequency element at odd index
-        for (int i = 1; i < n; i = i + 2) {
-            odd[nums[i]]++;
-            if (odd[nums[i]] > odd_max_freq) {
-                odd_max_element = nums[i];
-                odd_max_freq = odd[nums[i]];
-            } else if (odd[nums[i]] <= odd_max_freq &&
-                       nums[i] != odd_max_element &&
-                       odd_sec_max_freq < odd[nums[i]]) {
-                odd_sec_max_element = nums[i];
-                odd_sec_max_freq = odd[nums[i]];
-            }
-        }
-        if (even_max_element != odd_max_element) {
-            return n - even_max_freq - odd_max_freq;
-        } else {
-            return min(n - odd_max_freq - even_sec_max_freq,
-                       n - even_max_freq - odd_sec_max_freq);
-        }
-        return 0;
+
+        for(auto i:odd) if(i.first!=oval) somax=max(i.second,somax);
+        for(auto i:even) if(i.first!=eval) semax=max(i.second,semax);
+
+        
+        if(oval!=eval) return  nums.size()-omax-emax;  
+        return min(nums.size()-somax-emax,nums.size()-omax-semax);
     }
 };

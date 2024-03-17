65bff9e32224bc565e1a6714d0df0df29829fd37
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-03-17 09:42:01 +0530

    Time: 123 ms (10.22%) | Memory: 20.2 MB (8.78%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,20 +1,32 @@
+#include <string>
+#include <map>
+#include <queue>
+using namespace std;
+
 class Solution {
 public:
     int minDeletions(string s) {
-        map<char,int>cnt;
-        unordered_set<int>frq;
-        int del=0;
-for(auto it : s){
-    cnt[it]++;
-}
-for(auto i : cnt){
-    int fq=i.second;
-    while(fq>0 && frq.find(fq)!=frq.end()){
-        fq--;
-        del++;
-    }
-    frq.insert(fq);
-}
-return del;
+        map<char,int> mp;
+        for(int i = 0; i < s.size(); i++) {
+            mp[s[i]]++;
+        }
+        int c = 0;
+        priority_queue<int> pq;
+        for(auto it : mp) {
+            pq.push(it.second);
+        }
+        while(!pq.empty()) {
+            int tp1 = pq.top();
+            pq.pop();
+            if(!pq.empty()) {
+                int tp2 = pq.top();
+                if(tp1 == tp2 && tp1 > 0) {
+                    c++;
+                    tp1--;
+                    pq.push(tp1);
+                }
+            }
+        }
+        return c;
     }
 };

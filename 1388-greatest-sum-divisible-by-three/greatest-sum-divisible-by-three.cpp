3b7c0ed2e4010fce54b9c4d738f9915cc3628099
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-03-22 18:40:28 +0530

    Time: 62 ms (32.70%) | Memory: 44.5 MB (21.66%) - LeetSync

---
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+
+    int solve(const vector<int>& nums, int i, int mod) {
+        if (i >= nums.size()) {
+            if (mod == 0)
+                return 0;
+            return INT_MIN; // representing negative infinity
+        }
+        
+        if (dp[i][mod] != -1)
+            return dp[i][mod];
+
+        int take = nums[i] + solve(nums, i + 1, (mod + nums[i]) % 3);
+        int notTake = solve(nums, i + 1, mod);
+
+        return dp[i][mod] = max(take, notTake);
+    }
+
+    int maxSumDivThree(vector<int>& nums) {
+        dp = vector<vector<int>>(nums.size(), vector<int>(3, -1));
+        return solve(nums, 0, 0);
+    }
+};

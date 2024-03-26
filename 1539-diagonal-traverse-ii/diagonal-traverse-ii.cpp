22e502f639cf04157dcec481ef90008adad7596a
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-03-26 10:26:24 +0530

    Time: 200 ms (34.86%) | Memory: 110.3 MB (19.77%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
@@ -1,25 +1,33 @@
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
         unordered_map<int, vector<int>> groups;
-        
-        for (int row = nums.size() - 1; row >= 0; row--) {
+
+        // Loop starting from the first row
+        for (int row = 0; row < nums.size(); row++) {
             for (int col = 0; col < nums[row].size(); col++) {
                 int diagonal = row + col;
                 groups[diagonal].push_back(nums[row][col]);
             }
         }
-        
+
         vector<int> ans;
         int curr = 0;
-        
+
         while (groups.find(curr) != groups.end()) {
-            for (int num : groups[curr]) {
+            vector<int> in = groups[curr];
+            reverse(in.begin(), in.end());
+            for (int num : in) {
                 ans.push_back(num);
             }
             curr++;
         }
-        
         return ans;
     }
 };

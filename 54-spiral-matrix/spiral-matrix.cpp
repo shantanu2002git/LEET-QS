e2e3fb4d8aba5aa085c3ac4067203402bf36aa3c
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-03-31 23:59:49 +0530

    Time: 3 ms (36.80%) | Memory: 8.2 MB (53.30%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& mat) {
-        int n=mat.size(),m=mat[0].size();
-        int lef=0,rig=m-1, top=0,bot=n-1;
-
-        int direc=1;
-        vector<int>res;
-        while(lef<=rig && top<=bot){
-            if(direc==1){
-                for(int i=lef; i<=rig; i++){
-                    res.push_back(mat[top][i]);
-                }
-                top++;
-                direc=2;
-            }
-            else if(direc==2){
-                for(int i=top; i<=bot; i++){
-                    res.push_back(mat[i][rig]);
-                }
-                rig--;
-                direc=3;
-            }
-            else if(direc==3){
-                for(int i=rig; i>=lef; i--){
-                    res.push_back(mat[bot][i]);
-                }
-                bot--;
-                direc=4;
-            }
-            else if(direc=4){
-                for(int i=bot; i>=top; i--){
-                    res.push_back(mat[i][lef]);
-                }
-                lef++;
-                direc=1;
-            }
-        }
+        vector<int> res;
+        spiralOrderRecursive(mat, 0, mat.size() - 1, 0, mat[0].size() - 1, 1, res);
         return res;
+    }
+    
+    void spiralOrderRecursive(vector<vector<int>>& mat, int top, int bot, int lef, int rig, int direc, vector<int>& res) {
+        if (lef > rig || top > bot)
+            return;
+        
+        if (direc == 1) {
+            for (int i = lef; i <= rig; i++) {
+                res.push_back(mat[top][i]);
+            }
+            spiralOrderRecursive(mat, top + 1, bot, lef, rig, 2, res);
+        } else if (direc == 2) {
+            for (int i = top; i <= bot; i++) {
+                res.push_back(mat[i][rig]);
+            }
+            spiralOrderRecursive(mat, top, bot, lef, rig - 1, 3, res);
+        } else if (direc == 3) {
+            for (int i = rig; i >= lef; i--) {
+                res.push_back(mat[bot][i]);
+            }
+            spiralOrderRecursive(mat, top, bot - 1, lef, rig, 4, res);
+        } else if (direc == 4) {
+            for (int i = bot; i >= top; i--) {
+                res.push_back(mat[i][lef]);
+            }
+            spiralOrderRecursive(mat, top, bot, lef + 1, rig, 1, res);
+        }
     }
 };

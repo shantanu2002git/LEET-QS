3c76d5e21ecce07f95060b40d1f1ace03a18b1b5
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-04-04 08:21:49 +0530

    Time: 68 ms (92.35%) | Memory: 10 MB (71.36%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -1,20 +1,33 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class Solution {
-public:
-    int beautySum(string s) {
-        int n = s.size();
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            map<char, int> mp;
-            for (int j = i; j < n; j++) {
-                mp[s[j]]++;
-                int mx = -1e9, mn = 1e9;
-                for (auto& kv : mp) {
-                    mx = max(mx, kv.second);
-                    mn = min(mn, kv.second);
-                }
-                res += mx - mn;
+private:
+    void _max_min(int& mx, int& mi, vector<int>& fq) {
+        mi = INT_MAX;
+        mx = INT_MIN;
+        for (int i = 0; i < 26; i++) {
+            if (fq[i] != 0) {
+                mi = min(mi, fq[i]);
+                mx = max(mx, fq[i]);
             }
         }
-        return res;
+    }
+
+public:
+    int beautySum(string s) {
+        int n = s.size(), res = 0;
+        for (int i = 0; i < n; i++) {
+            vector<int> Al(26, 0);
+            for (int j = i; j < n; j++) {
+                Al[s[j] - 'a']++;
+                int mx, mi;
+                _max_min(mx, mi, Al);
+                res += mx - mi;
+            }
+        }
+        return res; //n*n*26
     }
 };

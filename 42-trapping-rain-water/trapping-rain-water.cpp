2714d35838d9fab940283eabcd48e64cd26ef1e7
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-04-12 12:06:25 +0530

    Time: 15 ms (21.13%) | Memory: 23.3 MB (13.23%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& h) {
+        int n = h.size();
+        // if (n <= 2) return 0; 
+        vector<int> prf(n), suf(n);
+        prf[0] = h[0];
+        for (int i = 1; i < n; i++) {
+            prf[i] = max(prf[i - 1], h[i]);
+        }
+        suf[n - 1] = h[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suf[i] = max(suf[i + 1], h[i]);
+        }
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res += min(prf[i], suf[i]) - h[i];
+        }
+        return res;
+    }
+};

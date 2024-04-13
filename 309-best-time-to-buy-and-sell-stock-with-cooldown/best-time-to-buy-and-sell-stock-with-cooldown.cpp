88bb5bd18edd12e2ec29bc71c69393c76d882aa8
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-04-13 12:38:55 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (16.35%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -7,15 +7,14 @@ if(i>=n){
 if(dp[i][by]!=-1){
     return dp[i][by];
 }
-// No need to take 
+
 int mx1=0,mx2=0;
-// mx1=call(i+1,1,n,p,dp);
 if(by==1){
     mx2=max(-p[i]+call(i+1,0,n,p,dp),0+call(i+1,1,n,p,dp));
 }else{
     mx2=max(+p[i]+call(i+2,1,n,p,dp), 0+call(i+1,0,n,p,dp));
 }
-return dp[i][by]=max(mx1,mx2);
+return dp[i][by]=mx2;
 
 }
     int maxProfit(vector<int>& p) {

0059b9ee28b2ad42ecf719c6cffeb41cc8a4c1bc
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-04-17 09:27:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (25.81%) - LeetSync

--- 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.cpp
+++ 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     bool squareIsWhite(string c) {
+        /*
         int rw=c[0]-'a',cl=c[1]-'0';
         if(((rw+1)%2!=0 && cl%2!=0) || ((rw+1)%2==0 && cl%2==0)){
             return 0;
         }
         cout<<rw+1<<cl;
         return 1;
+        */
+        return (c[0] + c[1]) % 2;
     }
 };

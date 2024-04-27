e6d433f50b734b29a124b615062babe800a78968
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-04-27 23:33:06 +0530

    Time: 76 ms (64.70%) | Memory: 70.1 MB (66.80%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-      int n=nums.size(),mx=-1,s=0,mn=-1e9;
-    for(int i=0; i<n; i++){
-        s=s+nums[i];
-        if(s>mx){
-            mx=max(mx,s);
+        int n = nums.size(), s = 0, mx = -1e9;
+int elemx=-1e9;
+        for (int i = 0; i < n; i++) {
+            s = s + nums[i];
+            if (s < 0) {
+                s = 0;
+            }
+            mx = max(s, mx);
+            elemx=max(elemx,nums[i]);
         }
-        else if(s<=0){
-            s=0;
-        }
-        mn=max(mn,nums[i]);
-    }
-    return max(mx,s)==0 ? mn : max(mx,s);
-    }
-};
-/*
-class Solution {
-public:  
-    int maxSubArray(vector<int>& nums) {
-        int max_sum = nums[0];
-        int sum = nums[0];
-        for (int i=1; i<nums.size(); i++)
-        {
-            sum = max(sum + nums[i], nums[i]);
-            max_sum = max(max_sum, sum);
-        }
-        return max_sum;
+        return mx==0 ? elemx :max(mx,elemx);
     }
 };
-
-*/

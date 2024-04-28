75724438d068df23d79b5a5c24cab859bdf51e61
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-04-28 16:07:27 +0530

    Time: 3 ms (98.66%) | Memory: 14 MB (29.81%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,17 +1,22 @@
-//#include <unordered_map>
- 
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
-       
-        for(int i = 0; i < nums.size(); i++){
-            if(mp.find(target - nums[i]) == mp.end())
-                mp[nums[i]] = i;
-            else
-                return {mp[target - nums[i]], i};
+        unordered_map<int, int> mp; // Map to store number-index pairs
+
+    
+        for (int i = 0; i < nums.size(); i++) {
+            int extra = target - nums[i];
+            if (mp.find(extra) != mp.end()) {
+                return {mp[extra], i};
+            }
+            mp[nums[i]] = i;
         }
- 
-        return {-1, -1};
+        
+     
+        return {};
     }
 };

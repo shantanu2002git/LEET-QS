3797d3a29264f1fd6f3e66cb68206552f8a8d717
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-05-14 11:51:34 +0530

    Time: 6 ms (63.60%) | Memory: 9.7 MB (93.79%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
 set<vector<int>>st;
-void calper(int idx, vector<int>&nums, vector<vector<int>>&res){
-    if(idx==nums.size()){
-        if(st.find(nums)==st.end()){
+void calper(int st, vector<int>&nums, vector<vector<int>>&res){
+    if(st==nums.size()){
+        // if(st.find(nums)==st.end()){
             res.push_back(nums);
-        }
-        st.insert(nums);
+        // }
+        // st.insert(nums);
         return;
     }
 
-    for(int i=idx; i<nums.size(); i++){
-        swap(nums[idx],nums[i]);
-        calper(idx+1,nums,res);
-        swap(nums[idx],nums[i]);
+    for(int i=st; i<nums.size(); i++){
+         bool found = false;
+        for (int j = st; j < i; j++)
+        {
+            if (nums[i] == nums[j])
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            swap(nums[st], nums[i]);
+            calper(st+1,nums,res);
+            swap(nums[st], nums[i]);
+        }
+       
     }
 }
     vector<vector<int>> permuteUnique(vector<int>& nums) {

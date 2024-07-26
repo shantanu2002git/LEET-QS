39b435defca9e1e1164eb2146afbaa923b35c65d
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-07-26 23:14:23 +0530

    Time: 50 ms (11.13%) | Memory: 20.5 MB (8.62%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,65 +1,62 @@
-
-
 class Solution {
 public:
-    // Function to perform BFS and count reachable cities within
-    int bfs(int startCity, int n, vector<vector<pair<int, int>>>&adj,int distanceThreshold) {
-        vector<int> dist(n, INT_MAX);
-        queue<int> q;
-        q.push(startCity);
-        dist[startCity] = 0;
-        int reachableCount = 0;
+    vector<pair<int, int>> adj[105];
+    vector<pair<int, int>> ans;
 
-        while (!q.empty()) {
-            int city = q.front();
-            q.pop();
+    bool static cmp(const pair<int, int> p1, const pair<int, int> p2) {
+        if (p1.first != p2.first)
+            return p1.first < p2.first;
 
-            for (const auto& neighbor : adj[city]) {
-                int nextCity = neighbor.first;
-                int weight = neighbor.second;
+        return p1.second > p2.second;
+    }
 
-                if (dist[city] + weight < dist[nextCity]) {
-                    dist[nextCity] = dist[city] + weight;
-                    q.push(nextCity);
+    void dijkstra(int src, int n, int distanceThreshold) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
+        vector<int> distance(n + 1, INT_MAX);
+
+        distance[src] = 0;
+        pq.push({0, src});
+
+        while (!pq.empty()) {
+            int prev = pq.top().second;
+            int prevDist = pq.top().first;
+            pq.pop();
+
+            for (auto it : adj[prev]) {
+                int next = it.first;
+                int nextDist = it.second;
+                if (distance[next] > distance[prev] + nextDist) {
+                    distance[next] = distance[prev] + nextDist;
+                    pq.push({distance[next], next});
                 }
             }
         }
 
-        // Count reachable cities within distanceThreshold
-        for (int i = 0; i < n; ++i) {
-            if (dist[i] <= distanceThreshold && dist[i] != INT_MAX) {
-                reachableCount++;
-            }
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (i != src && distance[i] <= distanceThreshold)
+                cnt++;
         }
 
-        return reachableCount;
+        ans.push_back({cnt, src});
     }
+
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        // Create adjacency list
-        vector<vector<pair<int, int>>> adj(n);
-        for (const auto& edge : edges) {
-            adj[edge[0]].push_back({edge[1], edge[2]});
-            adj[edge[1]].push_back({edge[0], edge[2]});
+
+        for (int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0], v = edges[i][1], w = edges[i][2];
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
         }
 
-        vector<pair<int, int>> cityReachCount; // (city, reachableCount)
-
-        // Perform BFS from each city to determine reachable cities within the
-        // threshold
-        for (int i = 0; i < n; ++i) {
-            int reachableCount = bfs(i,n,adj,distanceThreshold);
-            cityReachCount.push_back({i, reachableCount});
+        for (int i = 0; i < n; i++) {
+            dijkstra(i, n, distanceThreshold);
         }
 
-        // Sort cities first by number of reachable cities (ascending), then by
-        // city index (descending)
-        sort(cityReachCount.begin(), cityReachCount.end(),
-             [&](const pair<int, int>& p1, const pair<int, int>& p2) {
-                 return p1.second == p2.second ? p1.first > p2.first
-                                               : p1.second < p2.second;
-             });
+        sort(ans.begin(), ans.end(), cmp);
 
-        return cityReachCount[0].first; // Return the city with the smallest
-                                        // number of reachable cities
+        return ans[0].second;
     }
 };

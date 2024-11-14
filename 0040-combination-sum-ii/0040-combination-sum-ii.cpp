71950a5eb8071870eb85b326028aae3710e3c507
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-11-14 14:15:55 +0530

    Time: 0 ms (100%), Space: 13.1 MB (35.13%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    
-    void call(vector<int>& can, int idx, int s, vector<int>& in) {
-        if (s == 0) { 
+
+    void call(vector<int>& can, vector<int>& in, int idx, int s) {
+        if (s == 0) {
             res.push_back(in);
             return;
         }
-
-        for (int i = idx; i >= 0; i--) {
-           
-            if (i < idx && can[i] == can[i + 1])
-                continue;
-
-            if (can[i] <= s) {  
-                in.push_back(can[i]);
-                call(can, i - 1, s - can[i], in);
-                in.pop_back();  // Backtrack
-            }
+        if (idx < 0 || s < 0) {
+            return;
         }
+        in.push_back(can[idx]);
+        call(can, in, idx - 1, s - can[idx]);
+        in.pop_back(); // Backtrack
+        int prevIdx = idx - 1;
+        while (prevIdx >= 0 && can[prevIdx] == can[idx]) {
+            prevIdx--;
+        }
+
+        // Recurse without including the current element, starting from previous
+        // unique element
+        call(can, in, prevIdx, s);
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end()); // Sort to manage duplicates
         vector<int> in;
-        sort(candidates.begin(), candidates.end());  
-        call(candidates, candidates.size() - 1, target, in);
+        call(candidates, in, candidates.size() - 1, target);
         return res;
     }
 };

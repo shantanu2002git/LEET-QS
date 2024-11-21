f627f4df989af7c6cf9a88059eee15f3af3a178e
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-11-21 17:54:35 +0530

    Time: 0 ms (100%), Space: 14.8 MB (48.98%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int n = nums.size();
+        int breakidx = -1;
+
+        // Find the first number that is smaller than the next one, starting from the end
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                breakidx = i;
+                break;
+            }
+        }
+
+        // If no such number exists, the array is in descending order, reverse it
+        if (breakidx == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        // Find the smallest number that is larger than nums[breakidx] from the end
+        for (int i = n - 1; i > breakidx; i--) {
+            if (nums[i] > nums[breakidx]) {
+                swap(nums[i], nums[breakidx]);
+                break;
+            }
+        }
+
+        // Reverse the elements after breakidx to get the smallest next permutation
+        sort(nums.begin() + breakidx + 1, nums.end());
     }
 };

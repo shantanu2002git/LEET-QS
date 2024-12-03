e82b243e6d3ac3b1e160b3cdd0ef65bde2abfba6
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2024-12-03 17:29:40 +0530

    Time: 59 ms (8.26%), Space: 111.3 MB (5.03%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.cpp
+++ 2924-find-champion-ii/2924-find-champion-ii.cpp
@@ -1,45 +1,46 @@
-class Solution
-{
+
+class Solution {
 public:
-    int findChampion(int n, vector<vector<int>> &edges)
-    {
+    int findChampion(int n, vector<vector<int>>& edges) {
         vector<int> adj[n];
-        vector<int> ind(n, 0);
-        int fnd = -1;
-        for (auto it : edges)
-        {
-            adj[it[1]].push_back(it[0]);
-            ind[it[1]]++;
+        vector<int> inDegree(n, 0);
+
+        // Build adjacency list and in-degree array
+        for (auto it : edges) {
+            adj[it[0]].push_back(it[1]);
+            inDegree[it[1]]++;
         }
+
         queue<int> q;
-        for (int i=0; i<n; i++)
-        {
-           
-            if (ind[i] == 0)
-            {
-              
+        int candidate = -1;
+
+        // Push nodes with in-degree 0 into the queue
+        for (int i = 0; i < n; i++) {
+            if (inDegree[i] == 0) {
+                if (candidate != -1) { // More than one source
+                    return -1;
+                }
+                candidate = i;
                 q.push(i);
-                fnd = i;
-                // break;
             }
         }
 
-        while (!q.empty())
-        {
-           
-            int tp = q.front();
+        // Perform BFS/Topological Sort
+        int processed = 0;
+        while (!q.empty()) {
+            int node = q.front();
             q.pop();
-            for (auto it : adj[tp])
-            {
-                
-                ind[it]--;
-                if (ind[it] == 0)
-                {
-                    q.push(it);
+            processed++;
+
+            for (auto neighbor : adj[node]) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) {
+                    q.push(neighbor);
                 }
             }
         }
-        // cout<<fnd;
-        return (q.size() == 0) ? fnd : -1;
+
+        // If all nodes are processed and there’s a unique candidate
+        return (q.size()==0) ? candidate : -1;
     }
 };

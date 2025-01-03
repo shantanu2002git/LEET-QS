ea8c1d1f32bf20bc0063ef04ead70299dc315691
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-01-03 15:36:52 +0530

    Time: 10 ms (13.2%), Space: 97 MB (26.39%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
@@ -3,15 +3,27 @@ public:
     int waysToSplitArray(vector<int>& nums) {
         int n = nums.size();
         vector<long long> pref(n, 0);
-     
-        long long sum = accumulate(nums.begin(), nums.end(), 0LL); // Use 0LL to ensure the sum is long long
-        pref[0] = nums[0];
+
+        long long sum = accumulate(nums.begin(), nums.end(),0LL); // Use 0LL to ensure the sum is long long
+   
         long long res = 0;
+        /*
+     pref[0] = nums[0];
         for (int i = 1; i < n; i++) {
             pref[i] = pref[i - 1] + nums[i];
             if (pref[i - 1] >= (sum - pref[i - 1])) {
                 res++;
             }
+        }
+*/
+
+//TC O(n) SC(1) 
+        long long bigo=nums[0];
+        for(int i=1; i<n; i++){
+            if(bigo>=(sum-bigo)){
+                res++;
+            }
+            bigo+=nums[i];
         }
 
         return res;

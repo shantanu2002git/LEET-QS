ff5680230ac5f23eb76d3db6f94b67d4bdc99c0e
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-01-03 10:53:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -1,20 +1,58 @@
-class Solution {
+class Solution
+{
 public:
-    int maxScore(string s) {
+    int maxScore(string s)
+    {
         int n = s.size();
-        int totone = 0, remone = 0;
-
-        for (int i = 0; i < n; i++) {
-            totone += (s[i] == '1');
+        vector<int> left(n, 0);
+        vector<int> right(n, 0);
+        left[0] = s[0] == '0';
+        for (int i = 1; i < n; i++)
+        {
+            if (s[i] == '0')
+            {
+                left[i] = left[i - 1] + 1;
+            }
+            else
+            {
+                left[i] = left[i - 1];
+            }
         }
-        int rs = INT_MIN;
-        int zerox = 0;
-        for (int i = 0; i < n; i++) {
-            zerox += (s[i] == '0');
-            remone += (s[i] == '1');
 
-            rs = max(rs, zerox + (totone - remone));
+        right[n - 1] = (s[n - 1] == '1');
+        bool fl0=0, fl1=0;
+        for (int i = n - 2; i >= 0; i--)
+        {
+            
+            if (s[i] == '1')
+            {
+                right[i] = right[i + 1] + 1;
+                fl1=1;
+            }
+            else
+            {
+                right[i] = right[i + 1];
+            }
+            if(fl1){
+                fl0=1;
+            }
         }
-        return rs;
+        /*
+                for (auto it : left)
+                {
+                    cout << it << " ";
+                }
+                cout << endl;
+                for (auto it : right)
+                {
+                    cout << it << " ";
+                }
+                */
+        int res = 0;
+        for (int i = 0; i < n; i++)
+        {
+            res = max(res, (left[i] + right[i]));
+        }
+        return (left[n-1]==0  || fl0)? res-1 : res;
     }
 };

82c736854de5ab087d7d85ac6e3bc0dbb9be5cfc
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-02-12 09:41:47 +0530

    Time: 101 ms (8.25%), Space: 65 MB (42.73%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -5,6 +5,10 @@ public:
         int res = 0;
         map<int, int> mps;
         sort(nums.begin(),nums.end());
+        /*
+        why we need the sorting think again : 63 = 9, 36 =9 : how do ?
+        you update map with 'mp[sum]=number'
+        */
         for (auto it : nums) {
             string si = to_string(it);
             int ds = 0;

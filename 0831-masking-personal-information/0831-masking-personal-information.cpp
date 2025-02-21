8d8f3c8acf21c1a280b66d36aa1ed3479392438a
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-02-21 09:21:16 +0530

    Time: 0 ms (100%), Space: 8 MB (58.2%) - LeetHub

--- 0831-masking-personal-information/0831-masking-personal-information.cpp
+++ 0831-masking-personal-information/0831-masking-personal-information.cpp
@@ -4,12 +4,12 @@ public:
         int n = s.size();
         transform(s.begin(), s.end(), s.begin(), ::tolower);
         int lsc = -1;
-        int phnum=0;
-        string sph="";
+        int phnum = 0;
+        string sph = "";
         for (int i = 0; i < n; i++) {
-            if(s[i]>='0' && s[i]<='9'){
+            if (s[i] >= '0' && s[i] <= '9') {
                 phnum++;
-                sph=sph+s[i];
+                sph = sph + s[i];
             }
             if (s[i] == '@') {
                 lsc = i;
@@ -23,24 +23,24 @@ public:
             return emil_1f;
         }
 
+        else {
+            int exlen = (phnum - 10);
+            n = sph.size();
+            switch (exlen) {
+            case 0:
+                return "***-***-" + sph.substr(n - 4);
+                break;
+            case 1:
+                return "+*-***-***-" + sph.substr(n - 4);
+                break;
+            case 2:
+                return "+**-***-***-" + sph.substr(n - 4);
+                break;
+            case 3:
+                return "+***-***-***-" + sph.substr(n - 4);
 
-        else{
-            int exlen=(phnum-10);
-            n=sph.size();
-            switch(exlen){
-                case 0 :
-                return "***-***-"+sph.substr(n-4);
-                break;
-                case 1:
-                return "+*-***-***-"+sph.substr(n-4);
-                break;
-                case 2: 
-                return "+**-***-***-" +sph.substr(n-4);
-                break;
-                case 3:
-                return "+***-***-***-"+sph.substr(n-4);
-                
-                default  : "NO";
+            default:
+                "NO";
             }
         }
         return "...";

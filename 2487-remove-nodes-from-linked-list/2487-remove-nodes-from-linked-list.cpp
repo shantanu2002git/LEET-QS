e8968f204ae907c62c47ac95b32d8f6d743965dc
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-03-05 14:29:14 +0530

    Time: 23 ms (34.88%), Space: 171.9 MB (48.59%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -12,30 +12,25 @@ public:
     }
 
     ListNode* removeNodes(ListNode* head) {
-        if (!head) return NULL;
+        if (!head)
+            return NULL;
 
         // Step 1: Reverse the linked list
         ListNode* revHead = reverse(head);
 
-        // Step 2: Create a new linked list based on the given condition
-        ListNode* dummy = new ListNode(-1); // Dummy node
-        ListNode* go = dummy;
-        ListNode* curr = revHead;
+        ListNode* nwlist = new ListNode(-1);
+        ListNode* going = nwlist;
 
-        int maxVal = curr->val; // Track maximum value encountered
-        while (curr) {
-            if (curr->val >= maxVal) { // Only keep nodes meeting the condition
-                maxVal = curr->val;
-                go->next = new ListNode(curr->val);
-                go = go->next;
+        int maxval = revHead->val;
+        while (revHead) {
+            if (revHead->val >= maxval) {
+                going->next = new ListNode(revHead->val) ;
+                maxval = revHead->val;
+                going = going->next;
             }
-            curr = curr->next;
+            revHead = revHead->next;
         }
-        ListNode* finalList = reverse(dummy->next);
 
-        // Free memory of dummy node
-        delete dummy;
-
-        return finalList;
+        return reverse(nwlist->next);
     }
 };

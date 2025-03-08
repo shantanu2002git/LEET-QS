4e92cdde0b77f91b51565a5903305acb09df6060
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-03-08 15:22:48 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (25.50%) - LeetSync

---
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
@@ -0,0 +1,30 @@
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1,
+                                    vector<vector<int>>& nums2) {
+        vector<vector<int>> res;
+        map<int, int> mp;
+
+        // Merge nums1 into the map
+        for (auto& it : nums1) {
+            mp[it[0]] += it[1];
+        }
+
+        // Merge nums2 into the map
+        for (auto& it : nums2) {
+            mp[it[0]] += it[1];
+        }
+
+        // Create the result vector from the map
+        for (auto& it : mp) {
+            res.push_back({it.first, it.second});
+        }
+
+        return res;
+    }
+};

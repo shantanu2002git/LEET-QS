b08b3c732bdde55cdbbdd0d3b9b7375389527056
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-03-11 17:21:36 +0530

    Time: 401 ms (6.74%), Space: 216.5 MB (5.55%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int i = 0, j = 0, n = nums.size();
         vector<int> res;
-        multiset<int> ms;
-
-        int i = 0, j = 0,n=arr.size();
+        multiset<int> mst;
         while (i < n) {
-            ms.insert(arr[i]);
-            if (ms.size() == k) {
+            mst.insert(nums[i]);
+            if (mst.size() == k) {
 
-                int val = *(--ms.end());
-                res.push_back(val);
-                ms.erase(ms.find(arr[j]));
+                auto it = mst.rbegin(); 
+                // reverse sort format, big element infront()
+                res.push_back(*it);
+                mst.erase(mst.find(nums[j]));
+                /*
+                mst.erase(nums[j]);
+This removes all occurrences of nums[j] from multiset.
 
+
+mst.erase(mst.find(nums[j]));
+Only removes one instance of nums[j], leaving other occurrences untouched.
+                */
                 j++;
             }
+
             i++;
         }
         return res;

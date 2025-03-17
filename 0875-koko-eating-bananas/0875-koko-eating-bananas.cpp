2df985ad1c065d12b5fbd18501e5d31eaa4f7ab9
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-03-17 08:48:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,24 +1,34 @@
 class Solution {
-public://https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/
-    long long cal(vector<int>& arr, int div) {
-        long long si = 0;
-        for(int i = 0; i < arr.size(); i++) {
-            si = si + ceil((double)arr[i] / div);
-        }
-        return si;
-    }
-    
-    int minEatingSpeed(vector<int>& nums, int h) {
-        int st = 1, en = *max_element(nums.begin(), nums.end());
-        while (st <= en) {
-            int mid = (st + en) / 2;
-            long long vl = cal(nums, mid);
-            if (vl > h) {
-                st = mid + 1;
+public:
+
+/*
+https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/
+ 
+https://leetcode.com/problems/minimum-time-to-repair-cars
+
+*/
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int mxk = *max_element(piles.begin(), piles.end());
+        int mink = 1, res = mxk;
+
+        while (mink <= mxk) {
+            int md = (mink + mxk) / 2;
+            int sumh = 0;
+
+            for (int i = 0; i < piles.size(); i++) {
+                sumh += ceil((double)piles[i] / md);
+            }
+
+            if (sumh > h) { 
+              
+                mink = md + 1;
             } else {
-                en = mid - 1;
+                // Speed is valid, try smaller values
+                res = md;
+                mxk = md - 1;
             }
         }
-        return st;
+
+        return res;
     }
 };

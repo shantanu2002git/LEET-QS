0f374b8c53572ab741fb2199991a0bc38dd2af6e
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-03-19 22:12:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+       
+        if(s.empty()){
+            return 0;
+        }
+        sort(g.begin(),g.end());
+        sort(s.begin(), s.end());
+        int n=g.size(),m=s.size(),i=0,j=0,res=0;
+        while(i<n && j<m){
+            if(g[i]>s[j]){
+                j++;
+            }else{
+                res++;
+                i++;
+            }
+        }
+        return res;
+    }
+};

04cc3a94f5a1972bdff74e765ab3950804c3cd77
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-05-02 10:55:05 +0530

    Time: 0 ms (100%), Space: 7.9 MB (64.65%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-int dig(int num){
-   int d=0;
-   while(num>0){
-       int r=num%10;
-       d=d+(r*r);
-       num=num/10;
-   } 
-   return d;
-}
+    int dig(int num) {
+        int d = 0;
+        while (num > 0) {
+            int r = num % 10;
+            d = d + (r * r);
+            num = num / 10;
+        }
+        return d;
+    }
     bool isHappy(int n) {
-        int sum=n;
-        while(sum!=1){
-            sum=dig(sum);
-            if(sum==1) return true;
-            else if(sum==4) return false;
+        int sum = n;
+        while (sum != 1) {
+            sum = dig(sum);
+            if (sum == 1)
+                return true;
+            else if (sum == 4)
+                return false;
         }
         return true;
     }

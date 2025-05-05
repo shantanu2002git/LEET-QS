36bcb4333108c27fa2070908804bd2a613ff2045
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-05-05 19:28:13 +0530

    Time: 31 ms (78.04%), Space: 8.1 MB (30.89%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
     int res = 0;
-    void fun(int ele, int n, vector<int>& vis) {
-        if (ele > n) {
+    void call(int idx, vector<int>& num, int n) {
+        if (idx >= n) {
+            // cout<<"in..";
+            // bool valid = true;
+            // for (int i = 1; i <= n; i++) {
+            //     if ((num[i - 1] % (i)) != 0 && ((i) % num[i - 1]) != 0) {
+            //         valid = false;
+            //         break;
+            //     }
+            // }
+            // if (valid) {
+            //     res++;
+            // }
+
             res++;
+            return;
         }
-        for (int i = 1; i <= n; i++) {
-            if (!vis[i] && (ele % i == 0 || i % ele == 0)) {
-                vis[i] = true;
-                fun(ele + 1, n, vis);
-                vis[i] = false;
+
+        for (int l = idx; l < n; l++) {
+            swap(num[idx], num[l]);
+            /*
+            
+            we can keep checking the elements while being added to the permutation array at every step for the divisibility condition and can stop creating it any further as soon as we find out the element just added to the permutation violates the divisiblity condition.
+            
+            */
+            if ((num[idx] % (idx + 1)) == 0 || ((idx+1) % num[idx]) == 0) {
+                call(idx + 1, num, n);
             }
+            swap(num[idx], num[l]);
         }
     }
     int countArrangement(int n) {
-        vector<int> vist(n + 1, 0);
-        fun(1, n, vist);
+        vector<int> num;
+        for (int i = 1; i <= n; i++) {
+            num.push_back(i);
+        }
+        call(0, num, n);
         return res;
     }
 };

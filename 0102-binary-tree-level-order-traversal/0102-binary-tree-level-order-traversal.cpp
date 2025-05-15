ffcde05bdd2ade900423631ce0e4e3112dce1d7e
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-05-15 19:07:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -6,34 +6,28 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        if(root==NULL){
-            return {};
-        }
-        queue<TreeNode*>q;
-        vector<vector<int>>res;
-        q.push(root);
-        while(!q.empty()){
-            vector<int>in;
-            int n=q.size();
-            for(int i=0; i<n; i++){
-                auto it =q.front(); q.pop();
-                in.push_back(it->val);
-                if(it->left){
-                    q.push(it->left);
-                }
-                if(it->right){
-                    q.push(it->right);
-                }
-            }
+    vector<vector<int>> res;
+    vector<int> in;
+   void call(TreeNode* root, int lev) {
+        if (in.size() == lev) {
             res.push_back(in);
+            in.clear();
+            return;
         }
-        return res;
 
+        in.push_back(root->val);
+        call(root->left, lev + 1);
+        call(root->right, lev + 1);
+    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        int lev = 0;
+         call(root, lev);
+         return res;
     }
 };

059d19cc8f0e0707efa30ae0cf325bafe67164cd
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-05-16 12:04:27 +0530

    Time: 0 ms (100%), Space: 12.3 MB (40.05%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool call(int ele, vector<int>& cit) {
+        int i = 0, j = cit.size() - 1;
+
+        while (i <= j) {
+            int md = (i + j) / 2;
+            if (cit[md] >= ele) {
+                j = md - 1;
+            } else {
+                i = md + 1;
+            }
+        }
+
+        return (cit.size() - i >= ele);
+    }
+
+    int hIndex(vector<int>& cit) {
+        sort(cit.begin(), cit.end());
+        int res = 0, n = cit.size();
+        for (int i = 1; i <= n; i++) {
+            if (call(i, cit)) {
+                res = i;
+            }
+        }
+        return res;
+    }
+};
+
+
+/*
+[3,0,6,1,5]
+
+1 : 3 6 5
+2 : 3 6 5
+3 : 3 6 5
+
+4 : 5 6
+
+sort : 0 1 3 5 6   n=5 
+l    |    r  | md  | ele = 1
+0         5  | (0+5)/2 : 2 :  3  
+
+
+
+if (index<=(count_greater_element)){
+    res++;
+}
+
+
+*/

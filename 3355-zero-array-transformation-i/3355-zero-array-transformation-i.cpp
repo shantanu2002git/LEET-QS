9e51d099f3e4406719767e36b04b0534fc1f728e
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-05-20 09:02:34 +0530

    Time: 953 ms (5.03%), Space: 325.4 MB (22.43%) - LeetHub

---
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& q) {
+        int n = nums.size();
+        vector<int> diff(n, 0);
+
+        for (auto it : q) {
+            diff[it[0]] += -1;
+            if (it[1] < n - 1) {
+                diff[it[1] + 1] += 1;
+            }
+        }
+        for(int i=1; i<n; i++){
+            diff[i]=diff[i-1]+diff[i];
+        }
+        for(auto it  : diff){
+            cout<<it<< " ";
+        }
+        for(int i=0; i<n; i++){
+            if((nums[i]+diff[i])>0){
+                return 0;
+            }
+        }
+        return 1;
+    }
+};

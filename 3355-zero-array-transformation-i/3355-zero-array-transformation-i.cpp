7597368267adc1804c8cf7a8fa5927ca0c41cb4c
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-05-20 09:02:54 +0530

    Time: 950 ms (5.03%), Space: 325.4 MB (22.43%) - LeetHub

--- 3355-zero-array-transformation-i/3355-zero-array-transformation-i.cpp
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.cpp
@@ -3,21 +3,30 @@ public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& q) {
         int n = nums.size();
         vector<int> diff(n, 0);
+/*
+question like this index_i, index_j, XOR k
+to implement this :
 
-        for (auto it : q) {
+arr[index_i]^=k
+arr[index_j+1]^=k
+
+because XOR of XOR clancle out for "K"
+
+*/
+        for (auto  it : q) {
             diff[it[0]] += -1;
             if (it[1] < n - 1) {
                 diff[it[1] + 1] += 1;
             }
         }
-        for(int i=1; i<n; i++){
-            diff[i]=diff[i-1]+diff[i];
+        for (int i = 1; i < n; i++) {
+            diff[i] = diff[i - 1] + diff[i];
         }
-        for(auto it  : diff){
-            cout<<it<< " ";
+        for (auto it : diff) {
+            cout << it << " ";
         }
-        for(int i=0; i<n; i++){
-            if((nums[i]+diff[i])>0){
+        for (int i = 0; i < n; i++) {
+            if ((nums[i] + diff[i]) > 0) {
                 return 0;
             }
         }

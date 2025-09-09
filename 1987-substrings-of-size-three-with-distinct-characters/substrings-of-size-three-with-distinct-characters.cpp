30343096af87f2ac511c43bb407dd42c679c5dc5
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-09-09 23:29:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (50.97%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int countGoodSubstrings(string s) {
-        int n = s.size();
-        int i = 0, j = 0, res=0;
-        map<char, int> mp;
-        while (i < n) {
-            mp[s[i]]++;
-           
-            while (i - j + 1 > 3) {
-                mp[s[j]]--;
-                if (mp[s[j]] == 0) {
-                    mp.erase(s[j]);
-                }
-                j++;
-            }
-             if (mp.size() == 3 && i - j + 1 == 3) {
+        int res = 0, n = s.size();
+        if (n < 3) {
+            return 0;
+        }
+        char a = s[0], b = s[1], c=s[2];
+        for (int i = 3; i < n; i++) {
+            if (a != b && b != c && c != a) {
                 res++;
             }
-            i++;
+            a = b;
+            b = c;
+            c = s[i];
+        }
+        if (a != b && b != c && c != a) {
+            res++;
         }
         return res;
     }

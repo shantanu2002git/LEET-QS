6203a0cb4114f6272064e0e368b3be8c6f6e99ec
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-09-22 23:06:11 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (31.22%) - LeetSync

---
+++ 1184-car-pooling/car-pooling.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        sort(trips.begin(), trips.end(),
+             [](vector<int>& a, vector<int>& b) { return a[1] < b[1]; });
+
+        vector<int> vis(1001, 0);
+        for (auto it : trips) {
+            vis[it[1]] += it[0];
+            vis[it[2]] -= it[0];
+        }
+        int ps = 0;
+        for (auto l : vis) {
+            ps += l;
+            if (ps > capacity) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+};

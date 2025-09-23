4e392ae8a5607ddd6184f0ca1c8cff84d1e21a49
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2025-09-23 08:41:12 +0530

    Time: 8 ms (26.46%) | Memory: 24.8 MB (15.89%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intv) {
-        sort(intv.begin(), intv.end());
+    vector<vector<int>> merge(vector<vector<int>>& inv) {
+
         vector<vector<int>> res;
-        int prvls = 0;
-        for (int i = 0; i < intv.size(); i++) {
+        sort(inv.begin(), inv.end());
+        for (auto it : inv) {
             if (res.empty()) {
-                res.push_back(intv[i]);
+                res.push_back(it);
             } else {
-                if (intv[i][0] <= res.back()[1]) {
-                    res.back()[1] = max(res.back()[1], intv[i][1]);
+                if (res.back()[1] >= it[0]) {
+                    res.back()[1] = max(res.back()[1], it[1]);
+
                 } else {
-                    res.push_back(intv[i]);
+                    res.push_back(it);
                 }
             }
         }

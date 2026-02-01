6df2afd2f0291e4851e695f545ac2f7c4b6f4a89
shantanu sahoo <135010107+shantanu2002git@users.noreply.github.com>
2026-02-01 12:07:26 +0530

    Time: 4 ms (4.52%), Space: 30.4 MB (74.87%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
     int minimumCost(vector<int>& nums) {
-        sort(nums.begin()+1, nums.end());
-        return nums[0]+nums[1]+nums[2];
+        // sort(nums.begin()+1, nums.end());
+        int n1 = 100, n2 = 100, n = nums.size();
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < n1) {
+                   n2=n1;
+                n1 = nums[i];
+            }
+           else if ( nums[i]<n2 ) {
+                n2 = nums[i];
+            }
+        }
+        cout<<nums[0]<<" "<<n1<<" "<<n2;
+        return nums[0] + n1 + n2;
     }
 };
